08463eed98dd77b7a5657e77d39c592dab4ec0fa
Oliver Plummer <oliver.plummer@outlook.com>
2023-11-28 13:12:32 +1100

    Update

--- index.ts
+++ index.ts
@@ -1,18 +1,18 @@
-import { TSESLint } from "@typescript-eslint/utils"
+import type { TSESLint } from "@typescript-eslint/utils";
 
 const configuration: TSESLint.Linter.Config = {
-	rules: {
-		"@graphql-eslint/input-name": "off",
-		"@graphql-eslint/alphabetize": "off",
-		"@graphql-eslint/naming-convention": "off",
-		"@graphql-eslint/strict-id-in-types": "off",
-		"@graphql-eslint/no-typename-prefix": "off",
-		"@graphql-eslint/require-description": "off",
-		"@graphql-eslint/no-hashtag-description": "off",
-		"@graphql-eslint/match-document-filename": "off",
-		"@graphql-eslint/no-scalar-result-type-on-mutation": "off",
-		"@graphql-eslint/require-field-of-type-query-in-mutation-result": "off",
-	},
-}
+  rules: {
+    "@graphql-eslint/input-name": "off",
+    "@graphql-eslint/alphabetize": "off",
+    "@graphql-eslint/naming-convention": "off",
+    "@graphql-eslint/strict-id-in-types": "off",
+    "@graphql-eslint/no-typename-prefix": "off",
+    "@graphql-eslint/require-description": "off",
+    "@graphql-eslint/no-hashtag-description": "off",
+    "@graphql-eslint/match-document-filename": "off",
+    "@graphql-eslint/no-scalar-result-type-on-mutation": "off",
+    "@graphql-eslint/require-field-of-type-query-in-mutation-result": "off",
+  },
+};
 
-module.exports = configuration
+export default configuration;
